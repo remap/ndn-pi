e80089f39f0ad976af512d156c63e732372b2f1f
Spencer Sutterlin <ssutterlin1@ucla.edu>
2014-04-30 20:04:29 +0000

    Backup unrelated files

---
+++ backup/compile_stuff.sh
@@ -0,0 +1,30 @@
+#cd ~/ndn/ndnx
+#git pull
+#./configure
+#make
+#sudo make install
+
+#cd ~/ndn/ndn-cpp
+#git pull
+#./configure
+#make
+#sudo make install
+
+#cd ~/ndn/ndn-cpp-dev
+#git pull
+#./waf configure
+#./waf
+#sudo ./waf install
+
+#cd ~/ndn/ndnd-tlv
+#git pull
+#./waf configure
+#./waf
+#sudo ./waf install
+
+cd ~/ndn/NFD
+git pull
+./waf configure
+./waf
+sudo ./waf install
+
---
+++ backup/init_repos.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/named-data/ndnx
+git clone https://github.com/named-data/ndn-cpp
+git clone https://github.com/named-data/ndn-cpp-dev
+git clone https://github.com/named-data/ndnd-tlv
+git clone https://github.com/named-data/NFD
+git clone https://github.com/named-data/PyNDN2
+
+sudo apt-get install build-essential
+sudo apt-get install openssl expat libpcap-dev
+sudo apt-get install libssl-dev libsqlite3-dev libcrypto++-dev libboost1.48-all-dev
