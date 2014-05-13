3021f25bcc4b7d447a76db85b46cb2bc5505f65c
Spencer Sutterlin <ssutterlin1@ucla.edu>
2014-05-13 21:47:21 +0000

    Bug fixes to publish_pir

--- tests/publish_pir.py
+++ tests/publish_pir.py
@@ -8,6 +8,7 @@ import subprocess
 from os.path import expanduser, join
 import socket
 from sensors.pir import Pir
+import struct
 
 def getSerial():
     with open('/proc/cpuinfo') as f:
@@ -30,7 +31,9 @@ class PirDataLogger:
     def __init__(self, data_interval):
         # connect to local repo
         self.publisher = RepoSocketPublisher(12345)
-        self.prefix = Name("/home/dev/" + getSerial() + "/pir/0/data")
+        self.serial = getSerial()
+        self.prefix = Name("/home/dev/" + self.serial + "/pir/0/data")
+        self.commandInterestFace = Face("localhost")
         self.interval = data_interval
         self.pir = Pir()
         
@@ -43,10 +46,9 @@ class PirDataLogger:
         # print 'Use key name ' + str(self.key_name) + ' to sign data'
         # self.si = pyccn.SignedInfo(self.key.publicKeyID, pyccn.KeyLocator(self.key_name))
 
-    def publishData(self, payload):
-        timestamp_ms = int(time.time() * 1000) # in milliseconds
-        timestamp_ms_packed = struct.pack('!Q', timestamp_ms)
-        data = Data(self.prefix.append(timestamp_ms_packed))
+    def publishData(self, payload, timestamp):
+        timestampPacked = struct.pack('!Q', timestamp)
+        data = Data(self.prefix.append(timestampPacked))
 
         content = json.dumps(payload)
         data.setContent(content)
@@ -62,22 +64,38 @@ class PirDataLogger:
         dump("named ", self.prefix.append(timestamp_ms_packed))
         print self.prefix # TODO: make sure self.prefix is "/home/dev/<serial>/pir/0/data"
 
+    def expressDataReadyCommandInterest(self, name):
+        interest = Interest(Name("/home/all/command/dataready/dataready/dev/" + self.serial + "/pir/0/data/"))
+        interest.setInterestLifetimeMilliseconds(3000)
+        # TODO: Start timer
+        self.commandInterestFace.expressInterest(interest, counter.onData, expressDataReadyCommandInterest(name))
+        # TODO: probably shouldn't directly call expressDataReadyCommandInterest on timeout (= infinite loop if fail)
+
+    def onData(self, interest, data):
+        # TODO: how to tweak the onData function to receive interests
+        # Whoop-de-friggin-do
+        pass
+
     def run(self):
         # Publish first packet regardless of change
         prevPirVal = pirVal = self.pir.read()
         payload = {'pir':pirVal}
-        self.publishData(payload)
+        timestamp = int(time.time() * 1000) # in milliseconds
+        self.publishData(payload, timestamp)
         sample_count = 1
+        timestampPacked = struct.pack('!Q', timestamp)
+        expressDataReadyCommandInterest("/dev/" + self.serial + "/pir/0/data/" + timestampPacked)
 
         while (True):
             pirVal = self.pir.read()
-            if prevPirVal != pirVal
+            if prevPirVal != pirVal:
                 payload = {'pir':pirVal}
+                timestamp = int(time.time() * 1000) # in milliseconds
                 self.publishData(payload)
                 sample_count += 1
                 
                 # TODO: Express command interest
-                # TODO: Start timer
+                expressDataReadyCommandInterest("/dev/" + self.serial + "/pir/0/data/" + timestampPacked)
             
             time.sleep(self.interval)
 
