4193dffe380ad42b45b72a73a7afe340caa9b2ea
Spencer Sutterlin <ssutterlin1@ucla.edu>
2014-05-17 00:53:35 +0000

    Update nfd_prep script

--- nfd_prep.sh
+++ nfd_prep.sh
@@ -3,7 +3,16 @@ sudo cp /usr/local/etc/ndn/nfd.conf.sample /usr/local/etc/ndn/nfd.conf
 sudo cp /usr/local/etc/ndn/client.conf.sample /usr/local/etc/ndn/client.conf
 
 # Generate keys and establish root of trust
-ndnsec-keygen /`whoami` | ndnsec-install-cert -
 sudo mkdir -p /usr/local/etc/ndn/keys
+ndnsec-keygen /`whoami` | ndnsec-install-cert -
 ndnsec-cert-dump -i /`whoami` > default.ndncert
 sudo mv default.ndncert /usr/local/etc/ndn/keys/default.ndncert
+
+# Generate keys for app
+#serial=`cat /proc/cpuinfo | grep "Serial" | awk -F":" '{print $2}' | tr -d ' '`
+#ndnsec-keygen /home/dev/$serial | ndnsec-install-cert -
+#ndnsec-cert-dump -i /home/dev/$serial | sudo ndnsec-install-cert - # Also install as root cert because sensor publisher must run as sudo
+#ndnsec-cert-dump -i /home/dev/$serial > default.ndncert
+#sudo mv default.ndncert /usr/local/etc/ndn/keys/default.ndncert
+
+#ndnsec-set-default /pi
