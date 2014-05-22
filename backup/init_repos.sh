799fbdf041d1a29cda1bdca5e92289fb4cfb8cda
Spencer Sutterlin <ssutterlin1@ucla.edu>
2014-05-22 23:32:02 +0000

    Update backup/init_repos

--- backup/init_repos.sh
+++ backup/init_repos.sh
@@ -1,11 +1,15 @@
-git clone https://github.com/named-data/ndnx
-git clone https://github.com/named-data/ndn-cpp
-#git clone https://github.com/named-data/ndn-cxx
-#git clone https://github.com/named-data/NFD
-#git clone https://github.com/named-data/repo-ng
-git clone git://github.com/named-data/PyNDN2
-git clone git://github.com/remap/ndn-pi
-
 sudo apt-get install build-essential
 sudo apt-get install openssl expat libpcap-dev
 sudo apt-get install libssl-dev libsqlite3-dev libcrypto++-dev libboost-all-dev
+
+git clone https://github.com/named-data/ndnx.git
+git clone https://github.com/named-data/ndn-cpp.git
+git clone https://github.com/named-data/PyNDN2.git
+
+# Not needed on pi if cross-compiling
+#git clone https://github.com/named-data/ndn-cxx.git
+#git clone https://github.com/named-data/NFD.git
+#git clone https://github.com/named-data/repo-ng.git
+
+echo "export PYTHONPATH=$PYTHONPATH:$HOME/ndn/PyNDN2/python:$HOME/ndn/ndn-pi" >> ~/.bashrc
+echo "ipv6" >> /etc/modules
