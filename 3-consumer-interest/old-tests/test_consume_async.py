7c8cee613ea20e980c2e9ce8be187e52fbd5ee8b
Spencer Sutterlin <ssutterlin1@ucla.edu>
2014-05-23 23:42:19 +0000

    Update test_consume_async, merge Counter and Consumer classes

--- 3-consumer-interest/old-tests/test_consume_async.py
+++ 3-consumer-interest/old-tests/test_consume_async.py
@@ -1,47 +1,47 @@
 import asyncio
 from pyndn import Name
 from pyndn import Interest
+from pyndn import Exclude
 from pyndn import ThreadsafeFace
 
 import logging
 logging.basicConfig(filename="async.log", level=logging.INFO)
 
-
-class Counter(object): # ResponseHandler
+class Consumer(object):
     def __init__(self):
         self._callbackCount = 0
         self._stopped = False
+        self._loop = asyncio.get_event_loop()
+        self._face = ThreadsafeFace(self._loop, "localhost")
+        self._exclude = Exclude()
+        self._pirStatuses = {}
 
     def onData(self, interest, data):
         self._callbackCount += 1
         logging.info("Data: " + data.getName().toUri())
         logging.info("Content: " + data.getContent().toRawStr())
+        timeComponent = data.getName().get(3)
+        if not self._exclude.matches(timeComponent):
+            self._exclude.appendComponent(timeComponent)
 
     def onTimeout(self, interest):
         self._callbackCount += 1
         logging.info("Timeout interest: " + interest.getName().toUri())
 
-class Consumer(object):
-    def __init__(self):
-        self._loop = asyncio.get_event_loop()
-        self._face = ThreadsafeFace(self._loop, "localhost")
-        self._counter = Counter()
-        self._pirStatuses = {}
-
     def express_interest_and_repeat(self, loop):
-        logging.info("Counter's callbackCount: " + str(self._counter._callbackCount))
+        logging.info("Counter's callbackCount: " + str(self._callbackCount))
         # Express interest here
         interest = Interest(Name("/home/pir/00000000d1f2533912"))
-        # interest.setExclude(exclude)
-        interest.setInterestLifetimeMilliseconds(2000.0)
+        interest.setExclude(self._exclude)
+        interest.setInterestLifetimeMilliseconds(10000.0)
         logging.info("Send interest: " + interest.getName().toUri())
-        # print "exclude:", interest.getExclude().toUri()
-        self._face.expressInterest(interest, self._counter.onData, self._counter.onTimeout)
+        logging.info("Exclude: " + interest.getExclude().toUri())
+        self._face.expressInterest(interest, self.onData, self.onTimeout)
         loop.call_later(0.25, self.express_interest_and_repeat, loop) # there is call_soon_threadsafe but no call_later_threadsafe
         
 
     def run(self):
-        self._face.stopWhen(lambda: self._counter._callbackCount >= 10)
+        self._face.stopWhen(lambda: self._callbackCount >= 10)
         self._loop.call_soon(self.express_interest_and_repeat, self._loop) # might need _threadsafe
         # Run until stopWhen stops the loop.
         self._loop.run_forever()
