1c77b628b53cf17a9a7cd7e35f4ea38dd2b105b9
Adeola Bannis <thecodemaiden@gmail.com>
2014-09-19 22:06:38 +0000

    Currently do not need the list message type

---
+++ ndn_pi/commands/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['cert_request_pb2', 'configure_device_pb2', 'list_devices_pb2', 
+  'update_capabilities_pb2']
+
+from cert_request_pb2 import CertificateRequestMessage
+from update_capabilities_pb2 import UpdateCapabilitiesCommandMessage
+from configure_device_pb2 import DeviceConfigurationMessage
